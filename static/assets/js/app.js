6aeced8ce065ebde4db40cd43ef1af383e4124d3
Antonieta Torres <54754043+atorresgolf@users.noreply.github.com>
2024-07-08 17:44:52 -0300

    volvimos atras para no usar pythonanywhere porque nos da erro

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -1,5 +1,5 @@
-// const URL = 'http://127.0.0.1:5000/'
-const URL = 'http://atorresgolf.pythonanywhere.com/'
+const URL = 'http://127.0.0.1:5000/'
+// const URL = 'http://atorresgolf.pythonanywhere.com/'
 // const tabla_inmuebles = document.getElementById('tabla_inmuebles');
 
 fetch(URL + 'inmuebles')
